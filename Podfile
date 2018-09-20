4de56c28403fac222fca043c9db5db6049eb23b9
sculs <liusongscu@gmail.com>
2018-09-20 22:23:35 +0200

    Database & firebase

---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TrevTravel' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+	pod 'Firebase/Core'
+	pod 'Firebase/Firestore'
+
+
+  # Pods for TrevTravel
+
+  target 'TrevTravelTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'TrevTravelUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- TrevTravel/TravelList.swift
+++ TrevTravel/TravelList.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class TravelList: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
@@ -15,7 +16,42 @@ class TravelList: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hi")
+        
+        // Get data from the database of firebase
+        let db = Firestore.firestore()
+        db.collection("users").whereField("name", isEqualTo: "abc").getDocuments { (snapshot, error) in
+            if error != nil {
+                print(error ?? "error 1")
+            } else {
+                for document in (snapshot?.documents)! {
+                    if let name = document.data()["name"] as? String {
+                        if let age = document.data()["age"] as? Int {
+                            print(name, age)
+                        }
+                    }
+                    
+                    
+                }
+            }
+        }
+        
+        // Add data to database
+        let name = "xyz"
+        let age = 80
+        let dict :[String : Any] = ["name": name, "age": age]
+        
+        // Add data with specific ID
+        db.collection("users").document("newID").setData(dict)
+        
+        // Add data with random ID
+        // db.collection("users").addDocument(data: dict)
+        
+        ///////////////////////////////////////////////////////////////////
+        
+        
+        
+        
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
