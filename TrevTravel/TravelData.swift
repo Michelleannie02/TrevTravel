52c014e916a37f048bdb4ba0918d54efbe97ecdd
Yangshan Liu <clarissa323@gmail.com>
2018-09-22 00:26:00 +0200

    Load data from Firebase for travel tableView

--- TrevTravel/TravelData.swift
+++ TrevTravel/TravelData.swift
@@ -7,59 +7,79 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
 
 class TravelData {
     
-    struct Content {
-        var image = ""
-        var text = ""
-        var isCoverImg = ""
-        var map = ""
-    }
-    
     struct TravelInfo {
-        var title = ""
+        // 9 fields
+        var author = ""
+        var changedAt = ""
+        var contentArray:Array<String> = []
         var coverImg = ""
-        var shortText = ""
+        var createdAt = ""
         var likes = ""
         var place = ""
-        var author = ""
-        var createdAt = ""
-        var changedAt = ""
-        var contentArray:[Content] = []
+        var shortText = ""
+        var title = ""
     }
     
     var travelArray:[TravelInfo] = []
-    var contentArray:[Content] = []
     
-    init() {
-        var newNote = TravelInfo()
-        newNote.title = "Drottningholm"
-        newNote.coverImg = "Drottningholm"
-        newNote.shortText = "The Drottningholm Palace (Swedish: Drottningholms slott) is the private residence of the Swedish royal family. It is located in Drottningholm."
-        newNote.likes = "3"
-        newNote.place = "Drottningholm 22"
-        newNote.author = "Yangshan"
-        newNote.createdAt = "2018-09-17"
-        travelArray.append(newNote)
-        travelArray.append(newNote)
-        travelArray.append(newNote)
-        travelArray.append(newNote)
-        
-        newNote.title = "Stadshuset"
-        newNote.coverImg = "Stadshuset"
-        newNote.shortText = "The Stockholm City Hall is the building of the Municipal Council for the City of Stockholm in Sweden. It stands on the eastern tip of Kungsholmen island, next to Riddarfjärden's northern shore and facing the islands of Riddarholmen and Södermalm"
-        newNote.likes = "1"
-        newNote.place = "Vasagatan 22"
-        newNote.author = "Yangshan"
-        newNote.createdAt = "2018-09-17"
-        travelArray.append(newNote)
-        travelArray.append(newNote)
-        travelArray.append(newNote)
-        travelArray.append(newNote)
+//    init() {
+//        var newNote = TravelInfo()
+//        newNote.title = "Drottningholm"
+//        newNote.coverImg = "Drottningholm"
+//        newNote.shortText = "The Drottningholm Palace (Swedish: Drottningholms slott) is the private residence of the Swedish royal family. It is located in Drottningholm."
+//        newNote.likes = "3"
+//        newNote.place = "Drottningholm 22, Stockholm, Sweden"
+//        newNote.author = "Yangshan"
+//        newNote.createdAt = "2018-09-17"
+//        travelArray.append(newNote)
+//        travelArray.append(newNote)
+//        travelArray.append(newNote)
+//        travelArray.append(newNote)
+//
+//        newNote.title = "Stadshuset"
+//        newNote.coverImg = "Stadshuset"
+//        newNote.shortText = "The Stockholm City Hall is the building of the Municipal Council for the City of Stockholm in Sweden. It stands on the eastern tip of Kungsholmen island, next to Riddarfjärden's northern shore and facing the islands of Riddarholmen and Södermalm"
+//        newNote.likes = "1"
+//        newNote.place = "Vasagatan 22, Stockholm, Sweden"
+//        newNote.author = "Yangshan"
+//        newNote.createdAt = "2018-09-17"
+//        travelArray.append(newNote)
+//        travelArray.append(newNote)
+//        travelArray.append(newNote)
+//        travelArray.append(newNote)
+//    }
+   
+    func loadDB(){
+        let db = Firestore.firestore()
+        db.collection("travelDiary").getDocuments { (querySnapshot, error) in
+            if let err = error {
+                print("Error getting documents from travelDiary: \(err)")
+            } else {
+                guard let qSnapshot = querySnapshot else {return}
+                var newDiary = TravelInfo()
+                for document in qSnapshot.documents {
+//                    newDiary.id = document.documentID
+                    newDiary.author = document.data()["author"] as? String ?? ""
+                    newDiary.changedAt = document.data()["changedAt"] as? String ?? ""
+                    newDiary.contentArray = document.data()["content"] as? Array ?? []
+                    newDiary.coverImg = document.data()["title"] as? String ?? ""
+                    newDiary.createdAt = document.data()["createdAt"] as? String ?? ""
+                    newDiary.likes = document.data()["likes"] as? String ?? ""
+                    newDiary.place = document.data()["place"] as? String ?? ""
+                    newDiary.shortText = document.data()["shortText"] as? String ?? ""
+                    newDiary.title = document.data()["title"] as? String ?? ""
+                    print("qSnapshot:",newDiary.author,newDiary.changedAt,newDiary.coverImg,newDiary.createdAt,newDiary.likes,newDiary.place,newDiary.shortText,newDiary.title,newDiary.contentArray,newDiary.contentArray.count)
+                    
+                    self.travelArray.append(newDiary)
+                }
+                print("TravelData class:",self.travelArray,self.travelArray.count)
+            }
+        }
     }
     
-    func addContent(){
-    
-    }
 }
--- TrevTravel/TravelList.swift
+++ TrevTravel/TravelList.swift
@@ -12,36 +12,64 @@ import Firebase
 class TravelList: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var travelTable: UITableView!
-    let travelData = TravelData()
+    
+    var travelData = TravelData()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Get all the travel diaries data from Firebase
+        print("in viewDidLoad.")
+        travelData.loadDB()
+        print("viewDidLoad, travelData.travelArray: ",travelData.travelArray.count)
+        
+//        let db = Firestore.firestore()
+//        db.collection("travelDiary").getDocuments { (querySnapshot, error) in
+//            if let err = error {
+//                print("Error getting documents from travelDiary: \(err)")
+//            } else {
+//                guard let qSnapshot = querySnapshot else {return}
+//                for document in qSnapshot.documents {
+//                    let id = document.documentID
+//                    let author = document.data()["author"] as? String ?? ""
+//                    let changedAt = document.data()["changedAt"] as? String ?? ""
+//                    let coverImg = document.data()["title"] as? String ?? ""
+//                    let createdAt = document.data()["createdAt"] as? String ?? ""
+//                    let likes = document.data()["likes"] as? String ?? ""
+//                    let place = document.data()["place"] as? String ?? ""
+//                    let shortText = document.data()["shortText"] as? String ?? ""
+//                    let title = document.data()["title"] as? String ?? ""
+//                    let contentArray = document.data()["content"] as? Array ?? []
+//                    print(id,author,changedAt,coverImg,createdAt,likes,place,shortText,title,contentArray)
+////                    print(id, author)
+//                }
+//            }
+//        }
+        
+        
         // Get data from the database of firebase
-        let db = Firestore.firestore()
-        db.collection("users").whereField("name", isEqualTo: "abc").getDocuments { (snapshot, error) in
-            if error != nil {
-                print(error ?? "error 1")
-            } else {
-                for document in (snapshot?.documents)! {
-                    if let name = document.data()["name"] as? String {
-                        if let age = document.data()["age"] as? Int {
-                            print(name, age)
-                        }
-                    }
-                    
-                    
-                }
-            }
-        }
+//        let db = Firestore.firestore()
+//        db.collection("users").whereField("name", isEqualTo: "abc").getDocuments { (snapshot, error) in
+//            if error != nil {
+//                print(error ?? "error 1")
+//            } else {
+//                for document in (snapshot?.documents)! {
+//                    if let name = document.data()["name"] as? String {
+//                        if let age = document.data()["age"] as? Int {
+//                            print(name, age)
+//                        }
+//                    }
+//                }
+//            }
+//        }
         
         // Add data to database
-        let name = "xyz"
-        let age = 80
-        let dict :[String : Any] = ["name": name, "age": age]
-        
+//        let name = "xyz"
+//        let age = 80
+//        let dict :[String : Any] = ["name": name, "age": age]
+//
         // Add data with specific ID
-        db.collection("users").document("newID").setData(dict)
+//        db.collection("users").document("newID").setData(dict)
         
         // Add data with random ID
         // db.collection("users").addDocument(data: dict)
