cbbff41722caa9ec03e5084bc2b586c089ac808c
Yangshan Liu <clarissa323@gmail.com>
2018-09-26 23:39:33 +0200

    Ui constraints fix

--- TrevTravel/TravelPage.swift
+++ TrevTravel/TravelPage.swift
@@ -137,8 +137,8 @@ class TravelPage: UIViewController, UIScrollViewDelegate, UITableViewDelegate, U
         
     }
     
-    @IBAction func chooseEdit() {
-    }
+//    @IBAction func chooseEdit() {
+//    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
