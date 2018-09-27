a911a2700e1004658fd5dfba9b0e4b6fb122a502
sculs <liusongscu@gmail.com>
2018-09-28 00:41:44 +0200

    uppload address

--- TrevTravel/NewTravel.swift
+++ TrevTravel/NewTravel.swift
@@ -85,7 +85,7 @@ class NewTravel: UIViewController, UITableViewDelegate, UITextViewDelegate, UITa
         } else {
             newTravelData.newTravelInfo.author = userEmail
             
-            newTravelData.newTravelInfo.place = "Vasagatan 22, Stockholm, Sweden" // Adjust
+            newTravelData.newTravelInfo.place = userDefault.string(forKey: "returnAddress")!
             newTravelData.newTravelInfo.shortText = newContent.text ?? ""
             newTravelData.newTravelInfo.title = newTitle.text ?? ""
             
